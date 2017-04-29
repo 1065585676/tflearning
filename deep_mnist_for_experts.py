6e00e0e0241b7726d05bbf6893de4dfb57a26a5e
1065585676 <1065585676@qq.com>
2017-04-29 14:07:37 +0800

    Deep MNIST For Experts Source Code.

---
+++ deep_mnist_for_experts.py
@@ -0,0 +1,8 @@
+# Load MNIST Data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+# Start TensorFlow InteractiveSession
+import tensorflow as tf
+sess = tf.InteractiveSession()
+
